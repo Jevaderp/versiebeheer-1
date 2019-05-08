92db9e8354d6ae9983b5a36253a87824424e35c8
Jevaderp <deditatedwam69@gmail.com>
2019-05-08 15:51:37 +0200

    Added class with field, object in index. Field called upon as well.

---
+++ class_text.php
@@ -0,0 +1,8 @@
+<?php
+
+    class Text {
+        // Fields
+        var $sometext = "This is some text.";
+    }
+
+?>
--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
+    include("./class_text.php");
 
-echo "Yeetus Deletus. <br> Some people just want to watch the world burn.";
+    echo "Yeetus Deletus. <br> Some people just want to watch the world burn. <hr>";
 
+
+    $text = new Text();
+
+    echo $text->sometext;
 ?>
