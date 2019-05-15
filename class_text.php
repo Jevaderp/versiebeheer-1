077298e28628d1b9eeed27965ebf362541f09457
Jevaderp <deditatedwam69@gmail.com>
2019-05-15 15:57:38 +0200

    Text3 field added to Text class on Underling branch

--- class_text.php
+++ class_text.php
@@ -4,7 +4,7 @@
         // Fields
         var $text1 = "This is text one.";
         var $text2 = "This is text two.";
-
+        var $text3 = "This is text three.";
         var $text4 = "This is text four on the new branch.";
     }
 
--- index.php
+++ index.php
@@ -8,6 +8,6 @@
 
     echo $text->text1 . "<br>";
     echo $text->text2 . "<br>";
-    echo "<br>";
+    echo $text->text3 . "<br>";
     echo $text->text4 . "<br>";
 ?>
